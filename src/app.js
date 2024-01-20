3209adb945720c236773ec57ac5439f5c8609b33
Arun Kumar <aruncode49@gmail.com>
2024-01-20 19:11:48 +0530

    custom api response and error handling (imp)

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// basic app configuration and middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 // configure dotenv
 dotenv.config();
 
 // connect mongodb
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT :: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`ERROR MONGODB CONNECTION FAILED :: ${err}`);
+  });
