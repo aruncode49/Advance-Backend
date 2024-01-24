8c0a7e91c15cb2a439c282228292d49e48773d2d
Arun Kumar <aruncode49@gmail.com>
2024-01-24 16:49:55 +0530

    video model

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // basically comes from cloudinary data
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
